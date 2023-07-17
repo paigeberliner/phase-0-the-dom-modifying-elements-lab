ead3dcb5c311602eb7c0a5bdda254eaba26b53ee
Paige <paigeberliner@gmail.com>
2023-07-17 12:47:53 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+
+document.body.appendChild(newHeader);
+
+newHeader.textContent = "Paige is the champion";
+
+
+
+
+
+
+
