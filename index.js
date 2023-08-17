87cd7b3b32b780e125bc416dde5324d406177291
Paige <paigeberliner@gmail.com>
2023-08-17 13:09:07 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const main = document.getElementById('main');
-main.remove();
+const removeMain = document.getElementById("main");
+removeMain.remove();
 
 const newHeader = document.createElement('h1');
-newHeader.setAttribute('id', 'victory');
+newHeader.id = 'victory';
 
 document.body.appendChild(newHeader);
 
 newHeader.textContent = "Paige is the champion";
-
-
+document.body.appendChild(newHeader);
 
 
 
